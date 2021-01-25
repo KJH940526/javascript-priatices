c9cf1135d0ffe2fee89a32e4be89ff1a9bf21fb8
kjh940526 <enddl0121@naver.com>
2021-01-25 15:20:51 +0900

    퍼스트

---
+++ ex01.js
@@ -0,0 +1,67 @@
+/*
+변수의 데이터 타입
+
+1-1.기본타입(primitve type), 유사객체
+    1) undefined
+    2) boolean
+    3) string
+    4) number
+1-2. funtion // new로 생성자로 만들어도 타입이 funtion임!
+1-3. 객체(object): new 생성자 함수를 사용해서 객체를 생성
+
+2. 자바스크립트 객체를 구분하는 또 다른 방법
+2-1 내장객체(Built-In, Native) 객체: 자바스크립트 엔진에 미리 정의되어 있는 객체
+Number, Boolean, Date, RegEx, ....와 같은 생성자 함수
+ParseInt(), setTimeout(), setInterval(), ...와 같은 
+전역객체(global, window)
+2-2. 호스트 객체
+    ex)호스트가 브라우저인 경우
+        document, location, xmlHttpRequset
+2-3. 사용자 객체
+    자바스크립트 코드가 엔진에서 실행되면서 실행되는 런타임 객체
+*/
+
+
+var u;
+var i = 10;
+var b = true;
+var s = "hello world";
+var f = function () {}
+var o = {};
+var a = [1,2,3];
+var n = null;
+
+console.log(u+ ":" + typeof(u));
+console.log(i+ ":" + typeof(i));
+console.log(b+ ":" + typeof(b));
+console.log(s+ ":" + typeof(s));
+console.log(f+ ":" + typeof(f));
+console.log(o+ ":" + typeof(o));
+console.log(a+ ":" + typeof(a));
+console.log(n+ ":" + typeof(n));
+
+console.log("=============================================");
+
+var i2 = new Number(10);
+var b2 = new Boolean(false);
+var s2 = new String("hello world");
+var f2 = new Function("a","b","return a + b"); //
+var o2 = new Object();
+var a2 = new Array([1,2,3]);
+
+console.log(i2+ ":" + typeof(i2));
+console.log(b2+ ":" + typeof(b2));
+console.log(s2+ ":" + typeof(s2));
+console.log(f2+ ":" + typeof(f2));
+console.log(o2+ ":" + typeof(o2));
+console.log(a2+ ":" + typeof(a2));
+
+
+//원시타입과 원시타입을 wrapper 객체는 별 구분없이 연산 가능하다.
+console.log(i+i2);
+console.log(s+s2);
+
+//원시 타입도 메소드 호출이 가능하다? 그 이유는 =>
+//원시타입에 메소드가 호출 될 때 임시객체가 만들어 져서 메소드를 호출한 후 사라진다. : 유사객체
+console.log(b.valueOf());
+console.log(new Boolean(b).valueOf());
---
+++ ex02.js
@@ -0,0 +1,32 @@
+/* null & undefined */
+
+var myVar1;
+var myVar2 = null;
+
+console.log(myVar1 + ":" + myVar2);
+console.log(typeof(myVar1)+ ":"+ typeof(myVar2));
+
+console.log(myVar1 == myVar2); //값비교     true
+console.log(myVar1 === myVar2); //타입비교  false
+
+console.log("=========================");
+
+// ==: equality, 값의 동치성, 형변환o
+console.log(1 == '1');
+console.log(true == 1);
+console.log('abc' == new String('abc'));
+
+console.log(true + 10);
+console.log(typeof('abc' + new String('abc'))); //string
+console.log(1 + '1'); //넘버가 스트링으로 형변환!! 11이 나옴!
+console.log('1' + 1);
+
+// ===: indentify, 객체의 동일성, 형변환 x
+console.log( 2 === '2');
+console.log(true === 1);
+console.log('abc' === new String('abc'));
+
+// 추천
+// 엄격하게 형변환을 해서 두 타입을 비교한다.
+var str = '5';
+console.log(parseInt(str) === 5);
